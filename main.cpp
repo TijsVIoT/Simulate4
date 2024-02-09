3f7f345af7cd51ee43f01c2fea559009c5b46ea6
TijsVIoT <150119462+TijsVIoT@users.noreply.github.com>
2024-02-09 16:09:45 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,19 +1,18 @@
-//#include <DFRobot_HX711_I2C.h>
-//#define HX711_I2C_ADDR
 #include <HX711.h>
-#include <Wire.h>
-
 
 // Definieer de pinnen waarop de HX711-module is aangesloten
-const int HX711_DOUT_PIN = 5;
-const int HX711_SCK_PIN = 6;
+const int HX711_DOUT_PIN = D5;
+const int HX711_SCK_PIN = D6;
 
-// Maak een HX711-object aan
-HX711 scale(HX711_DOUT_PIN, HX711_SCK_PIN);
+// Maak een HX711-object aan zonder argumenten voor de constructor
+HX711 scale;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("HX711 Weight Sensor Test");
+
+  // Initialiseer de HX711-module met de opgegeven pinnen
+  scale.begin(HX711_DOUT_PIN, HX711_SCK_PIN);
 
   // Kalibreer de weegschaal. Je moet een bekend gewicht plaatsen en de aflezing noteren
   scale.set_scale();
