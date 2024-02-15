2ee2ec2a281ec27822b6c4331d8b13fabc2ce434
TijsVIoT <150119462+TijsVIoT@users.noreply.github.com>
2024-02-15 11:31:47 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,32 +1,44 @@
-#include <HX711.h>
+//#include <DFRobot_HX711_I2C.h>
+//#define HX711_I2C_ADDR
+//#define HX711_I2C_ADDR 0x64
+#include <DFRobot_HX711_I2C.h>
+#define HX711_I2C_ADDR 0x64 // Replace 0x64 with the correct I2C address
 
-// Definieer de pinnen waarop de HX711-module is aangesloten
-const int HX711_DOUT_PIN = D5;
-const int HX711_SCK_PIN = D6;
 
-// Maak een HX711-object aan zonder argumenten voor de constructor
-HX711 scale;
+DFRobot_HX711_I2C MyScale;
+
+//DFRobot_HX711_I2C MyScale(&Wire,/*addr=*/0x64);
+//DFRobot_HX711_I2C MyScale;
+
+float Weight = 0;
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("HX711 Weight Sensor Test");
-
-  // Initialiseer de HX711-module met de opgegeven pinnen
-  scale.begin(HX711_DOUT_PIN, HX711_SCK_PIN);
-
-  // Kalibreer de weegschaal. Je moet een bekend gewicht plaatsen en de aflezing noteren
-  scale.set_scale();
-  scale.tare(); // Reset de schaal naar 0
+  while (!MyScale.begin()) {
+    Serial.println("The initialization of the chip is failed, please confirm whether the chip connection is correct");
+    delay(1000);
+  }
+  //// Set the calibration weight when the weight sensor module is automatically calibrated (g)
+  MyScale.setCalWeight(50);
+  // Set the trigger threshold (G) for automatic calibration of the weight sensor module. When only the weight of the object on the scale is greater than this value, the module will start the calibration process
+  // This value cannot be greater than the calibration weight of the setCalWeight() setting
+  MyScale.setThreshold(30);
+  // Obtain the calibration value. The accurate calibration value can be obtained after the calibration operation is completed
+  Serial.print("the calibration value of the sensor is: ");
+  Serial.println(MyScale.getCalibration());
+  MyScale.setCalibration(MyScale.getCalibration());
+  delay(1000);
 }
 
 void loop() {
-  // Lees de gewichtswaarde
-  float weight = scale.get_units();
-
-  // Geef het gewicht weer op de seriële monitor
-  Serial.print("Weight: ");
-  Serial.print(weight);
-  Serial.println(" kg");
-
-  delay(1000); // Wacht 1 seconde voordat je opnieuw leest
+  Weight = MyScale.readWeight();
+  Serial.print("weight is: ");
+  if(Weight > 0.5){
+    Serial.print(Weight, 1);
+  }
+  else{
+    Serial.print(0, 1);
+  }
+  Serial.println(" g");
+  delay(1000); 
 }
