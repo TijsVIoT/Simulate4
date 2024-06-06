f4612bcdbd305d981b89e6cda2750c2f9e3b4656
TijsVIoT <150119462+TijsVIoT@users.noreply.github.com>
2024-06-06 19:39:52 +0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -3,20 +3,24 @@
 #include <UniversalTelegramBot.h>
 #include <ArduinoJson.h>
 #include <Wire.h>
-#include <HTTPClient.h>
 #include <DFRobot_HX711_I2C.h>
+#include <HTTPClient.h>  // Zorg ervoor dat je deze header toevoegt
 
 const char* ssid = "Galaxy S20 FE A601";
 const char* password = "jnna0341";
 const char* botToken = "6998324646:AAFoh6_mfoINcOzhpMPf9ZNMbT95-CY5YGA";
 const String chatId = "5444660794";
 
-#define HX711_I2C_ADDR 0x64 // Controleer of dit het juiste I2C-adres is
+// Gebruik de standaard definitie in de bibliotheek
+#define HX711_I2C_ADDR 0x64 
+
 DFRobot_HX711_I2C MyScale(&Wire, HX711_I2C_ADDR);
 
 float Weight = 0;
 unsigned long previousMillis = 0;
-const long interval = 1000; // Interval van 10 seconden (10 * 1000 milliseconden)
+const long interval = 1000; // Interval van 1 seconde (1 * 1000 milliseconden)
+
+String x;
 
 // Servo motor pin and object
 const int servoPin = A4; // Change this to the correct pin
@@ -24,7 +28,7 @@ Servo myServo;
 
 void sendTelegramMessage(const char* message) {
   if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
+    HTTPClient http;  // Gebruik de HTTPClient klasse
     String url = String("https://api.telegram.org/bot") + botToken + "/sendMessage?chat_id=" + chatId + "&text=" + message;
 
     http.begin(url);
@@ -42,6 +46,7 @@ void sendTelegramMessage(const char* message) {
     Serial.println("Not connected to WiFi, can't send message");
   }
 }
+
 
 void connectToWiFi() {
   Serial.print("Connecting to ");
@@ -96,6 +101,9 @@ void loop() {
     connectToWiFi();
   }
 
+  x = Serial.readString();
+  Serial.print(x);
+
   unsigned long currentMillis = millis();
 
   if (currentMillis - previousMillis >= interval) {
@@ -103,7 +111,7 @@ void loop() {
 
     Weight = MyScale.readWeight();
     Serial.print("weight is: ");
-    if (Weight > 1.5) {
+    if (Weight > 1.0) {
       Serial.print(Weight, 1);
     } else {
       Serial.print(0, 1);
@@ -112,17 +120,20 @@ void loop() {
 
     if (Weight < 5.0) {
       sendTelegramMessage("De voederbak is bijna leeg!");
+      sendTelegramMessage(String(Weight).c_str());
 
-      // Open the servo motor for 10 seconds
+     // Open the servo motor for 10 seconds
       myServo.write(90); // Adjust this value to the correct open position
-      delay(20000); // Wait for 10 seconds
+      delay(10000); // Wait for 10 seconds
       myServo.write(0); // Close the servo motor
+
     }
 
     if (Weight > 20.0) {
       sendTelegramMessage("De voederbak zit over zijn limiet!");
+      sendTelegramMessage(String(Weight).c_str());
     }
   }
 
-  delay(5000); // Voeg een korte vertraging toe om de loop te vertragen
+  delay(10000); // Voeg een korte vertraging toe om de loop te vertragen
 }
